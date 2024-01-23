6b157a54a3f1478afb5ffa320a0c0690f2897ba2
Alexander_I <alex.iablonski@gmail.com>
2024-01-23 23:43:14 +0100

    Add test 1.1

--- eureka-server/src/test/java/com/iablonski/micro/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/iablonski/micro/eurekaserver/EurekaServerApplicationTests.java
@@ -1,13 +1,26 @@
 package com.iablonski.micro.eurekaserver;
 
+import com.netflix.eureka.EurekaServerConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {"server.port=8081",
+        "eureka.client.register-with-eureka=false",
+        "eureka.client.fetch-registry=false"})
 class EurekaServerApplicationTests {
+
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Autowired
+    private EurekaServerConfig eurekaServerConfig;
 
     @Test
     void contextLoads() {
-    }
 
+    }
 }
