7cee981128b8368e746bc57f37ed930bd67b611b
Alexander_I <alex.iablonski@gmail.com>
2024-01-25 23:31:37 +0100

    Test Auto LB

--- eureka-client-2/src/main/java/com/iablonski/eurekaclient2/EurekaClient2Application.java
+++ eureka-client-2/src/main/java/com/iablonski/eurekaclient2/EurekaClient2Application.java
@@ -1,4 +1,4 @@
-package com.iablonski.eurekaclient;
+package com.iablonski.eurekaclient2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EurekaClientApplication {
+public class EurekaClient2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class, args);
+        SpringApplication.run(EurekaClient2Application.class, args);
     }
 
 }
--- eureka-server/src/test/java/com/iablonski/micro/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/iablonski/micro/eurekaserver/EurekaServerApplicationTests.java
@@ -1,26 +1,40 @@
 package com.iablonski.micro.eurekaserver;
 
-import com.netflix.eureka.EurekaServerConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource(properties = {"server.port=8081",
-        "eureka.client.register-with-eureka=false",
-        "eureka.client.fetch-registry=false"})
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestPropertySource(locations = "classpath:test.yml")
 class EurekaServerApplicationTests {
 
-    @Autowired
-    private ApplicationEventPublisher applicationEventPublisher;
+    @LocalServerPort
+    private int serverPort;
 
     @Autowired
-    private EurekaServerConfig eurekaServerConfig;
+    private EurekaServerApplication eurekaServerApplication;
+
+    @Value("${eureka.client.service-url.defaultZone}")
+    private String eurekaDefaultZone;
 
     @Test
     void contextLoads() {
+        // Проверяем, что порт сервера установлен как ожидалось
+        assertEquals(8761, serverPort);
+        assertEquals("http://localhost:8081/eureka", eurekaDefaultZone);
+        assertNotNull(eurekaServerApplication, "Контекст приложения не был загружен");
 
+
+// Проверяем, что Eureka-сервер отвечает на запросы
+//        RestTemplate restTemplate = new RestTemplate();
+//        String healthEndpoint = "http://localhost:" + serverPort + "/actuator/health";
+//        String healthResponse = restTemplate.getForObject(healthEndpoint, String.class);
+//        assertTrue(healthResponse.contains("UP")); // Пример проверки, что состояние здоровья - "UP"
     }
 }
