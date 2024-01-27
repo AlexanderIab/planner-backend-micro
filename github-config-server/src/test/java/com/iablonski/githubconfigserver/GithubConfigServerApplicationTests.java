a93fd3797e048fd64f2269391982b757ecb4b4ff
Alexander_I <alex.iablonski@gmail.com>
2024-01-27 13:29:35 +0100

    Create Remote GitGub config file

---
+++ github-config-server/src/test/java/com/iablonski/githubconfigserver/GithubConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iablonski.githubconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
