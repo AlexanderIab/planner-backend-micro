9f2ae84a3c95a661de5f50aec3386cf17ec2613e
Alexander_I <alex.iablonski@gmail.com>
2024-01-29 20:03:55 +0100

    Create Api Gateway for Planner App

--- planner-gateway/src/test/java/com/iablonski/planner/gateway/PlannerGatewayApplicationTests.java
+++ planner-gateway/src/test/java/com/iablonski/planner/gateway/PlannerGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.iablonski.apigateway;
+package com.iablonski.planner.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiGatewayApplicationTests {
+class PlannerGatewayApplicationTests {
 
     @Test
     void contextLoads() {
